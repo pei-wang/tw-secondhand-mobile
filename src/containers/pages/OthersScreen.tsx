f8e96b0f6f0c0aafdd413b46d728cca163950424
Wei He <weihe@CNweihe.local>
2017-08-16 10:58:39 +0800

    Wei - correct typo

--- src/containers/pages/OthersScreen.tsx
+++ src/containers/pages/OthersScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import { connect, DispatchProp } from 'react-redux'
-import { Button } from '../../components/Button/Button'
+import Button  from '../../components/Button/Button'
 import { NavigationActions } from 'react-navigation'
 
 const styles = StyleSheet.create({
@@ -17,10 +17,9 @@ class OthersScreen extends React.Component<DispatchProp<{}>, {}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Others Screen!</Text>
-        <input placeholder='商品名称'/>
-        <input placeholder='售价￥'/>
-        <Button>
+        <TextInput value='商品描述' style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}/>
+        <TextInput value='价格￥' style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}/>
+        <Button title='出售商品' onPress={() => {}}/>
       </View>
     )
   }
