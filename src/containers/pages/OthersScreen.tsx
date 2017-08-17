7ee3c86bc7c903f4e72510952343a59e841190f6
Wei He <weihe@CNweihe.local>
2017-08-17 09:09:43 +0800

    Wei - Add upload images unable to display

--- src/containers/pages/OthersScreen.tsx
+++ src/containers/pages/OthersScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { StyleSheet, TextInput, View, Image } from 'react-native'
 import { connect, DispatchProp } from 'react-redux'
 import Button  from '../../components/Button/Button'
 import { NavigationActions } from 'react-navigation'
@@ -15,11 +15,15 @@ const styles = StyleSheet.create({
 
 class OthersScreen extends React.Component<DispatchProp<{}>, {}> {
   render() {
+    const textfieldStyle = {height: 40, width: 250, marginBottom: 20, borderColor: 'gray', borderBottomWidth: 1};
+    const uploadDefault = require('../resources/uploading-archive.svg');
     return (
       <View style={styles.container}>
-        <TextInput value='商品名称' style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}/>
-        <TextInput value='售价￥' style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}/>
-        <TextInput value='添加描述...' multiline={true} numberOfLines = {6} style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}/>
+        <Image source={uploadDefault} style={{width: 100, height: 100}} />
+        <TextInput value='商品名称' style={textfieldStyle}/>
+        <TextInput value='售价￥' style={textfieldStyle}/>
+        <TextInput value='添加描述...' multiline={true} numberOfLines = {6}
+        style={{height: 100, width: 250, marginBottom: 20, borderColor: 'gray', borderWidth: 1}}/>
         <Button title='出售商品' onPress={() => {}}/>
       </View>
     )
