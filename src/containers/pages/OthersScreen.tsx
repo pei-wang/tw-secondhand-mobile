15b00d2fdda6b9b285699c1d13faf65a7d66d144
Wei He <weihe@CNweihe.local>
2017-08-17 11:48:29 +0800

    Wei - Add upload UI

--- src/containers/pages/OthersScreen.tsx
+++ src/containers/pages/OthersScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { StyleSheet, TextInput, View, Image } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native'
 import { connect, DispatchProp } from 'react-redux'
 import Button  from '../../components/Button/Button'
 import { NavigationActions } from 'react-navigation'
@@ -11,19 +11,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  uploader: {
+    width: '100%',
+    height: 140,
+    backgroundColor: '#F2F2F2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
 })
 
 class OthersScreen extends React.Component<DispatchProp<{}>, {}> {
   render() {
-    const textfieldStyle = {height: 40, width: 250, marginBottom: 20, borderColor: 'gray', borderBottomWidth: 1};
-    const uploadDefault = require('../resources/uploading-archive.svg');
+    const textfieldStyle = {fontSize: 13, color: 'lightgray', height: 40, width: 250, marginBottom: 20, borderColor: '#A4A4A4', borderBottomWidth: 1};
+    const uploadDefault = require('../resources/uploading-archive.png');
     return (
       <View style={styles.container}>
-        <Image source={uploadDefault} style={{width: 100, height: 100, backgroundColor: 'red', marginBottom: 20}} />
+        <View style={styles.uploader}>
+          <Text style={{color: 'lightgray', fontSize: 20, marginBottom: 10}}>点击上传图片</Text>
+          <Image source={uploadDefault} style={{width: 50, height: 50}} />
+        </View>
         <TextInput value='商品名称' style={textfieldStyle}/>
         <TextInput value='售价￥' style={textfieldStyle}/>
         <TextInput value='添加描述...' multiline={true} numberOfLines = {6}
-        style={{height: 100, width: 250, marginBottom: 20, borderColor: 'gray', borderWidth: 1}}/>
+        style={{fontSize: 13, color: 'lightgray', height: 100, width: 250, marginBottom: 20, borderColor: '#A4A4A4', borderWidth: 1}}/>
         <Button title='出售商品' onPress={() => {}}/>
       </View>
     )
